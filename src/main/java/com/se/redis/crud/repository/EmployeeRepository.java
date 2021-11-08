94ef3ca3494a2f68bab09a3dfe8382bee0b04904
Le Nhat Huy <lenhathuy2212000@gmail.com>
2021-11-08 13:25:40 +0700

    List crud

--- src/main/java/com/se/redis/crud/repository/EmployeeRepository.java
+++ src/main/java/com/se/redis/crud/repository/EmployeeRepository.java
@@ -5,38 +5,54 @@ import java.util.List;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
 
 import com.se.redis.crud.model.Employee;
 
 
 @Repository
 public class EmployeeRepository {
-	 private HashOperations hashOperations;
+//	 private HashOperations hashOperations;
+	private ListOperations listOperations;
 	 private RedisTemplate redisTemplate;
 	 
 	 public EmployeeRepository(RedisTemplate redisTemplate) {
-	        this.hashOperations = redisTemplate.opsForHash();
+//	        this.hashOperations = redisTemplate.opsForHash();
+		 	this.listOperations = redisTemplate.opsForList();
 	        this.redisTemplate = redisTemplate;
 	 }
 	 
 	 public void saveEmployee(Employee employee){
-		 hashOperations.put("EMPLOYEE", employee.getId(), employee);
+//		 hashOperations.put("EMPLOYEE", employee.getId(), employee);
+		listOperations.rightPush("EMPLOYEE_LIST", employee);
 	 }
 	 
 	 public List<Employee> findAll(){
-		 return hashOperations.values("EMPLOYEE");
+//		 return hashOperations.values("EMPLOYEE");
+		 return listOperations.range("EMPLOYEE_LIST", 0, -1);
 	 }
 	    
 	 public Employee findById(Integer id){
-		 return (Employee) hashOperations.get("EMPLOYEE", id);
-	  }
+//		 return (Employee) hashOperations.get("EMPLOYEE", id);
+		 
+       List<Employee> list = findAll();
+       for(Employee e : list) {
+           if(e.getId() == id)
+               return e;
+       };
+       return null;
+	 }
 	 
 	 public void update(Employee employee){
 		 saveEmployee(employee);
 	 }
 	 
 	 public void delete(Integer id){
-		 hashOperations.delete("EMPLOYEE", id);
+//		 hashOperations.delete("EMPLOYEE", id);
+		 listOperations.remove("EMPLOYEE_LIST", 1, findById(id));
 	 }
 	 
 	 
